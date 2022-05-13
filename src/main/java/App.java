f01a8cfbc10d15cf45fee0f74a1aa5a08d471eb5
11057051@email.ntou.edu.tw <3416Emerald>
2022-05-13 14:17:34 +0800

    Test Code for Jenkins

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,7 +11,7 @@ public class App {
   public static void main(String[] args) {
     GradeConverter converter = new GradeConverter();
 
-    for (int i = 0; i < 105; i++) {
+    for (int  i =0; i < 105; i++) {
       System.out.println(i + ";" + converter.convert(i));
     }
   }
